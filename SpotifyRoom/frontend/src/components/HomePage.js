342b36d421816167cd13db664c5d91b0d073fab6
Ron <ronald.agyepong@yahoo.com>
2022-07-21 19:56:30 -0600

    Use-Effect-Hook-And-Auto-Redirect

--- SpotifyRoom/frontend/src/components/HomePage.js
+++ SpotifyRoom/frontend/src/components/HomePage.js
@@ -1,31 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import JoinRoomPage from "./JoinRoomPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
 import {
     BrowserRouter as Router,
     Routes,
     Route,
     Link,
-    Redirect
+    Navigate
 } from "react-router-dom";
 
 
-export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-    }
+export default function HomePage() {
 
-    render() {
-        return (
-            <Router>
-                <Routes>
-                    <Route exact path="/" element={<p>This is the homepage</p>} />
-                    <Route path="/join" element={<JoinRoomPage />} />
-                    <Route path="/create" element={<CreateRoomPage />} />
-                    <Route path="/room/:roomCode" element={<Room />} />
-                </Routes>
-            </Router>
+    const [roomState, setRoomState] = useState({roomCode:null});
+
+    useEffect(() => {
+        fetch('/api/user-in-room')
+            .then((response) => response.json())
+            .then((data) => {
+                setRoomState({
+                    ...roomState,
+                    roomCode: data.code,
+                });
+            });
+    });
+
+    const renderHomePage = () => {
+        return(
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" compact="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to="/join" component={Link}>
+                            Join A Room
+                        </Button>
+                        <Button color="secondary" to="/create" component={Link}>
+                            Create A Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
         );
-    }
+    };
+
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={
+                    roomState.roomCode ? (<Navigate replace to={`/room/${roomState.roomCode}`} />) : (renderHomePage())
+                } />
+                <Route path="/join" element={<JoinRoomPage />} />
+                <Route path="/create" element={<CreateRoomPage />} />
+                <Route path="/room/:roomCode" element={<Room />} />
+            </Routes>
+        </Router>
+    );
 }
